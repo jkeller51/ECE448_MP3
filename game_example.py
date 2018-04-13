315ac88119a6e9b2f5c7287d76bc4b727d817de9
Jon Keller <j.keller51@live.com>
2018-04-13 16:18:50 -0500

    Update to gfx library

--- game_example.py
+++ game_example.py
@@ -9,7 +9,10 @@ if __name__ == '__main__':
     
     while 1:
         # main loop
-        window.update()
+        window.update() # this happens on a different thread
+        
+        print("We can do stuff without interfering with the gfx display rate", time.time())
+        
         if (time.time() > start+5 and time.time() < start+10):
             # example of moving the player down, ball right
             # window updates at 30 fps; keep this in mind
@@ -20,7 +23,6 @@ if __name__ == '__main__':
             # moving player up, ball left
             window.player.y -= 1
             window.ball.x -= 1
-            
         
         
 
