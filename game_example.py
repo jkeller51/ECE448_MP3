02a4a1d0f1aecb6243af5564ec217208f5135d70
Jon Keller <j.keller51@live.com>
2018-04-13 15:53:25 -0500

    Comment update

--- game_example.py
+++ game_example.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
         # main loop
         window.update()
         if (time.time() > start+5 and time.time() < start+10):
-            # example of moving the player up
+            # example of moving the player down, ball right
             # window updates at 30 fps; keep this in mind
             window.player.y += 1
             window.ball.x += 1   # 30 pixels/second
         
         if (time.time() > start+10 and time.time() < start+15):
-            # moving player down
+            # moving player up, ball left
             window.player.y -= 1
             window.ball.x -= 1
             
